4b85dec7d2bfc6be7f070a625c1ab87ea89b7baa
Asa designer web <94604570+aliakbarasa@users.noreply.github.com>
2022-04-04 18:30:13 +0430

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+class AtroposComponent extends HTMLElement {
+    constructor() {
+      super();
+    }
+
+    connectedCallback() {
+      this.atropos = new Atropos({
+        el: this.querySelector('.atropos'),
+        onEnter() {
+          console.log('Atropos Component: Enter');
+        },
+        onLeave() {
+          console.log('Atropos Component: Leave');
+        },
+        onRotate(x, y) {
+          console.log('Atropos Component: Rotate', x, y);
+        }
+      });
+
+      console.log('Atropos Component: Connected!', this);
+    }
+
+    disconnectedCallback() {
+      this.atropos.destroy();
+
+      console.log('Atropos Component: Disconnected!', this);
+    }
+  }
+
+  customElements.define('atropos-component', AtroposComponent);
